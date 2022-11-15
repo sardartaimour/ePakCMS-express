f84fc788f0255e3b2971431973deebe07ad54874
taimour-niche <taimour.nicheacademy@nxvt.com>
2022-11-15 18:21:09 +0500

    ....

---
+++ models/PatientVisit.js
@@ -0,0 +1,53 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+    const PatientAllergy = sequelize.define('PatientAllergy', {
+        allergy_id: {
+            type: DataTypes.INTEGER(10),
+            allowNull: false,
+            autoIncrement: true,
+            primaryKey: true,
+        },
+        allergy: {
+            type: DataTypes.STRING(150),
+            allowNull: false
+        },
+        description: {
+            type: DataTypes.STRING(200)
+        },
+        clinic_id: {
+            type: DataTypes.INTEGER(10),
+            allowNull: false
+        },
+        patient_id: {
+            type: DataTypes.INTEGER(10),
+            allowNull: false
+        }
+    }, {
+        freezeTableName: true,
+        tableName: 'cms_patient_allergies',
+        underscored: true,
+        timestamps: true,
+        createdAt: 'created_at',
+        updatedAt: 'updated_at',
+        indexes: [
+            {
+                fields: ['allergy']
+            }
+        ]
+    });
+    PatientAllergy.associate = function (models) {
+
+        PatientAllergy.belongsTo(models.Clinic, {
+			foreignKey: {
+				name: 'clinic_id',
+			},
+		});
+        PatientAllergy.belongsTo(models.Patient, {
+			foreignKey: {
+				name: 'patient_id',
+			},
+		});
+    };
+   
+    return PatientAllergy;
+};
---
+++ models/PhysicianNotes.js
@@ -0,0 +1,67 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+    const PhysicianService = sequelize.define('PhysicianService', {
+        physician_service_id: {
+            type: DataTypes.INTEGER(10),
+            allowNull: false,
+            autoIncrement: true,
+            primaryKey: true,
+        },
+        physician_id: {
+            type: DataTypes.INTEGER(10),
+            allowNull: false
+        },
+        clinic_id: {
+            type: DataTypes.INTEGER(10),
+            allowNull: false
+        },
+        service_title: {
+            type: DataTypes.STRING(150),
+            allowNull: false
+        },
+        service_cost: {
+            type: FLOAT(11,2),
+            allowNull: false,
+            defaultValue: 0.0
+        },
+        service_duration: {
+            type: DataTypes.INTEGER(10),
+            allowNull: false,
+            defaultValue: 0
+        },
+
+    }, {
+        freezeTableName: true,
+        tableName: 'cms_physician_services',
+        underscored: true,
+        timestamps: true,
+        createdAt: 'created_at',
+        updatedAt: 'updated_at',
+        indexes: [
+            {
+                fields: ['service_title', 'service_cost']
+            }
+        ]
+    });
+    PhysicianService.associate = function (models) {
+
+        PhysicianService.belongsTo(models.Employee, {
+			foreignKey: {
+				name: 'physician_id',
+			},
+		});
+        PhysicianService.belongsTo(models.Clinic, {
+			foreignKey: {
+				name: 'clinic_id',
+			},
+		});
+
+        PhysicianService.hasMany(models.Appointment, {
+            foreignKey: {
+                name: 'physician_service_id'
+            }
+        });
+    };
+   
+    return PhysicianService;
+};
